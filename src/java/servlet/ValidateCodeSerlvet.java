54895818d014c777d3b4980845cf728520ed8ea3
Apollo Team <truongthiuyentrang@gmail.com>
2021-07-08 20:54:17 +0700

    Cập nhật tính năng gửi mail
    Thêm tính năng gửi mail xác nhận, 2 thư viện được cập nhật hỗ trợ cho việc gửi mail, dự tính sẽ cập nhật giới hạn thời gian tồn tại của code

---
+++ src/java/servlet/ValidateCodeSerlvet.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author truon
+ */
+@WebServlet(name = "ValidateCodeSerlvet", urlPatterns = {"/validateCode"})
+public class ValidateCodeSerlvet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String email = request.getParameter("emailA");
+        String code  = (String) request.getSession().getAttribute("codeCheck");
+        String validateCode = request.getParameter("code");
+        if (code.trim().equals(validateCode.trim())) {
+            request.setAttribute("email", email);
+            request.getSession().removeAttribute("codeCheck");
+            request.getRequestDispatcher("Register").forward(request, response);
+        }else{
+            request.setAttribute("emailA", email);
+            request.setAttribute("MESSAGE", "Không khớp, vui lòng nhập lại");
+            request.getRequestDispatcher("validate.jsp").forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
